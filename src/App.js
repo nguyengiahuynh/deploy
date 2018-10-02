cbe5ae0f713346855e6ebbce3e1688503eeb45a2
1512214@student.hcmus.edu.vn <1512214@student.hcmus.edu.vn>
2018-10-02 09:42:00 +0700

    -done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,74 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 import logo from './logo.svg';
+import justifiedLayout from 'justified-layout'; 
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+
+
+const config = {
+  containerWidth: 1087,
+  containerPadding: 0,
+  boxSpacing: {
+    horizontal: 5,
+    vertical: 5
   }
 }
 
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      pictures: [],
+      geometry: null,
+      next: 1
+    }
+  }
+
+  editSizeImage(data){
+    return data.map((item) => {
+        return {width: +item.width_m, height: +item.height_m}
+    })
+  }
+
+  componentDidMount() { 
+    axios.get('https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=ca3783111609d69139840916b7a01ad2&format=json&nojsoncallback=1&per_page=20&page=1&extras=media%2Curl_m%2Cowner_name')
+      .then(res => {
+          this.setState({
+            pictures: res.data.photos.photo,
+            geometry: justifiedLayout(this.editSizeImage(res.data.photos.photo), config),
+            next: this.state.next + 1,
+          });
+          console.log(this.state.geometry);
+          })
+  }
+
+  render() {
+    return (
+      <div className="container">
+      {!!this.state.pictures.length && this.state.pictures.map((item, key) => {
+        return(
+          <div className="photo-view" key={key} style={this.state.geometry.boxes[key]}>
+            <div className="interaction-view">
+              <div className="photo-list-photo-interaction">
+                <a className="overlay"> </a>
+                <div className="interaction-bar">
+                    <div className="text">
+                      <a className="title">{item.title}</a>
+                      <a className="attribution">by {item.ownername} - {item.views} views</a>
+                    </div>
+                </div>
+              </div>
+            </div>
+            <img src={item.url_m} alt="img" />
+          </div>
+          )
+        })
+      }
+      </div>
+    )
+  }
+
+
+}
 export default App;
